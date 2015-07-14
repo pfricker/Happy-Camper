c0922fa3ce76c3247f23efe70aec37a38781533d
Paul Fricker <pfricker@gmail.com>
2015-07-14 15:18:36 -0400

    bug fixes

--- app/mailers/User_mailer.rb
+++ app/mailers/User_mailer.rb
@@ -4,8 +4,8 @@ class UserMailer < ApplicationMailer
   def request_mailer(user, gear, date1, date2)
     @user = user
     @gear = gear
-    @from = date1
-    @to = date2
+    @date1 = date1
+    @date2 = date2
     mail(to: @gear.user.email, subject: "#{@user.username.capitalize} would like to borrow some gear. ")
   end
 
