2f4e6475ef456b81d1b385f8622d974e87cae948
Paul Fricker <pfricker@gmail.com>
2015-07-21 11:09:42 -0400

    preliminary display for API data

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -3,8 +3,7 @@ class TripsController < ApplicationController
     zip = params["destination"]
     dates = params["date"]["start_date"][5..9] + params["date"]["end_date"][5..9]
     dates.delete!('^0-9')
-    response = HTTParty.get("http://api.wunderground.com/api/2d2aff62ce2b475a/planner_#{dates}/q/#{zip}.json")
-    binding.pry
+    @data = HTTParty.get("http://api.wunderground.com/api/2d2aff62ce2b475a/planner_#{dates}/q/#{zip}.json")
   end
 
 
